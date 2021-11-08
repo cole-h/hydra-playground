c3f4581eeab7525f91d204723d9261ba2e7edfe8
Cole Helbling <cole.e.helbling@outlook.com>
2021-11-08 11:06:20 -0800

    wip

--- default.nix
+++ default.nix
@@ -1,4 +1,16 @@
 let
   pkgs = import <nixpkgs> { };
+  hi = (pkgs.hello.overrideAttrs (_: {
+    a = "7";
+    # postFixup = "false";
+  }));
+  jobs = {
+    job = pkgs.releaseTools.aggregate {
+      name = "hello";
+      constituents = [
+        hi
+      ];
+    };
+  };
 in
-pkgs.hello
+jobs
