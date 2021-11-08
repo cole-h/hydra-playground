e7f9965a0733b099194e9132cddedf69ed5a0b87
Cole Helbling <cole.e.helbling@outlook.com>
2021-11-08 14:09:14 -0800

    bad commit

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   pkgs = import <nixpkgs> { };
   hi = (pkgs.hello.overrideAttrs (_: {
     a = "7";
-    # postFixup = "false";
+    postFixup = "false";
   }));
   jobs = {
     job = pkgs.releaseTools.aggregate {
