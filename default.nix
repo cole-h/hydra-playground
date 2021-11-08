2d5a5d369acb6a0d235bbce3c06be20926fdc060
Cole Helbling <cole.e.helbling@outlook.com>
2021-11-08 14:14:10 -0800

    maybe add package that succeeds to mark as pending?

--- default.nix
+++ default.nix
@@ -1,14 +1,24 @@
 let
   pkgs = import <nixpkgs> { };
   hi = (pkgs.hello.overrideAttrs (_: {
-    a = "7";
+    a = "11";
     preBuild = "sleep infinity";
+  }));
+  hi2 = (pkgs.hello.overrideAttrs (_: {
+    a = "10";
   }));
   jobs = {
     job = pkgs.releaseTools.aggregate {
       name = "hello";
       constituents = [
         hi
+        pkgs.hello
+      ];
+    };
+    job2 = pkgs.releaseTools.aggregate {
+      name = "hello2";
+      constituents = [
+        pkgs.hello
       ];
     };
   };
